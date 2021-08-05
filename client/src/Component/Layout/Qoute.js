a8b137863d9b0cf6e95841e7e2e901ea0b6cd01d
Knight Khode <elmirhamude@gmail.com>
2021-08-04 17:23:03 -0700

    added quote section

---
+++ client/src/Component/Layout/Qoute.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Qoute = () => {
+    return (
+        <div className="quote">
+            <div className="quote-content">
+                <div className="quotation">
+                    <h4 className="q-m">
+                        Loving God, Loving Others and Serving the World
+                    </h4>
+                    <div className="line"></div>
+                    <p className="q-w">
+                        “The church is the body of Christ on earth, empowered by the Holy Spirit to continue the task of reaching the lost and discipling the saved, helping them become fully devoted followers of Christ.”
+                        Ephesians 4:1-16
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Qoute
--- client/src/Component/Pages/Home.js
+++ client/src/Component/Pages/Home.js
@@ -1,12 +1,14 @@
 import React,{Fragment} from 'react'
 import Navbar from '../reuseables/Navbar'
 import ShowCase from '../Layout/ShowCase'
+import Qoute from '../Layout/Qoute'
 
 const Home = () => {
     return (
         <Fragment>
             <Navbar/>
             <ShowCase/>
+            <Qoute/>
         </Fragment>
     )
 }
