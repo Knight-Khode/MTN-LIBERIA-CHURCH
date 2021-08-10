8ff559d18aa529018501b8b38d79b3f39cc4ffcb
Knight Khode <elmirhamude@gmail.com>
2021-08-10 14:40:42 -0700

    added user sidebar

--- client/src/Component/Layout/userLayout/UserSideBar.js
+++ client/src/Component/Layout/userLayout/UserSideBar.js
@@ -2,8 +2,29 @@ import React from 'react'
 
 const UserSideBar = () => {
     return (
-        <div>
-            usersidebar
+        <div className="user-side-bar">
+           <div className="user-side-bar-header">
+                <div className="user-side-bar-header-content">
+                    <h4>Hello Kwame!</h4>
+                    <i class="fas fa-home"></i>
+                </div>
+           </div>
+           <div className="user-side-bar-body">
+                <ul>
+                    <li>
+                        <i class="fas fa-church"></i>
+                        <a href="#">Choose Branch</a>
+                    </li>
+                    <li>
+                        <i class="fas fa-money-check-alt"></i>
+                        <a href="#">Make Payment</a>
+                    </li>
+                    <li>
+                        <i class="fas fa-sign-out-alt"></i>
+                        <a href="#">Logout</a>
+                    </li>
+                </ul>
+           </div>
         </div>
     )
 }
--- client/src/Component/Pages/User.js
+++ client/src/Component/Pages/User.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import UserNavbar from '../Layout/userLayout/UserNavbar'
 import UserShowcase from '../Layout/userLayout/UserShowcase'
+import UserSideBar from '../Layout/userLayout/UserSideBar'
 
 const User = () => {
     return (
         <div>
             <UserNavbar/>
             <UserShowcase/>
+            <div className="userbody">
+                <div className="userbody-flex">
+                    <UserSideBar/>
+                </div>
+            </div>
         </div>
     )
 }
