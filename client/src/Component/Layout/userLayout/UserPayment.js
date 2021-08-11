6b86960de59ccb3b23e2e535d314a64ea52a2d88
Knight Khode <elmirhamude@gmail.com>
2021-08-10 17:13:32 -0700

    ui for payment info complete

---
+++ client/src/Component/Layout/userLayout/UserPayment.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const UserPayment = () => {
+    return (
+        <div className="user-payment">
+            <div className="user-payment-header">
+                <div className="user-payment-header-content">
+                    <h4>Payments</h4>
+                    <i class="fas fa-money-check-alt"></i>
+                </div>
+            </div>
+            <div className="user-payment-body">
+                <ul>
+                    <li className="payment-info">
+                        <div className="top-content top2">
+                            <i class="fas fa-paper-plane"></i>
+                            <p>Select Payment Type</p>
+                        </div>
+                        <div className="bottom-content">
+                            <div className="bottom-content-control">
+                                <i class="fas fa-arrow-right"></i>
+                                <p>Offering</p>
+                            </div>
+                            <div className="bottom-content-control">
+                                <i class="fas fa-arrow-right"></i>
+                                <p>Donation</p>
+                            </div>
+                            <div className="bottom-content-control">
+                                <i class="fas fa-arrow-right"></i>
+                                <p>Tithe</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="top-content">
+                            <i class="fas fa-paper-plane"></i>
+                            <p>Enter MOMO number</p>
+                        </div>  
+                    </li>
+                    <li>
+                        <div className="top-content">
+                            <i class="fas fa-paper-plane"></i>
+                            <p>Enter amount</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="top-content">
+                            <i class="fas fa-paper-plane"></i>
+                            <p>Click pay button</p>
+                        </div>
+                    </li>
+                </ul>
+                <div className="pay-form-btn">
+                    <button type="submit">Continue</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserPayment
--- client/src/Component/Pages/User.js
+++ client/src/Component/Pages/User.js
@@ -3,6 +3,7 @@ import UserNavbar from '../Layout/userLayout/UserNavbar'
 import UserShowcase from '../Layout/userLayout/UserShowcase'
 import UserSideBar from '../Layout/userLayout/UserSideBar'
 import Branches from '../Layout/userLayout/Branches'
+import UserPayment from '../Layout/userLayout/UserPayment'
 
 const User = () => {
     return (
@@ -13,6 +14,7 @@ const User = () => {
                 <div className="userbody-flex">
                     <UserSideBar/>
                     <Branches/>
+                    <UserPayment/>
                 </div>
             </div>
         </div>
