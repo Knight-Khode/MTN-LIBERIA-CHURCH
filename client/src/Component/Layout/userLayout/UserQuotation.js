36121b57c5cfc35cde7f3d855bc046aeb2ac9ae0
Knight Khode <elmirhamude@gmail.com>
2021-08-16 15:18:47 -0700

    added quotation card

--- client/src/Component/Layout/userLayout/UserQuotation.js
+++ client/src/Component/Layout/userLayout/UserQuotation.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import bible from '../../../img/bible.jpg'
 
 const UserQuotation = () => {
     return (
-        <div>
-            Quoatations
+        <div className="user-quotation">
+            <div className="quotation-img">
+                <img src={bible} alt="img"/>
+            </div>
+            <div className="quotation-info">
+                <div className="user-quotation-header">
+                    <h3>Daily Quotation</h3>
+                    <i class="fas fa-bible"></i>
+                </div>
+                <div className="user-quotation-body">
+                    <div className="verse-chapter">
+                        <p>Genesis 2:3</p>
+                    </div>
+                    <div className="user-quote">
+                        <p>My son attend to my words incline thine ears unto my sayings</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
